ee8522b61e2ff92c5d054486c323ac5af76a3748
StarshipladDev <starshipladdev@gmail.com>
2019-12-21 17:07:12 +1300

    Implemented Entity mechanical information in DropGrid.Core Entitys reference ActionCommand class (To be implemeted)

---
+++ DropGrid.Core/Entity/Entity.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DropGrid.Client.Entity
+{
+    /**
+     * <summary> Entity is a representation of a non-battlemap object displayed in game</summary>
+     */
+    abstract class Entity
+    {
+        private String name,assetID;
+        private int xPos, yPos;
+        private ActionCommand action;
+        public void setValues(String name, int xPos, int yPos)
+        {
+            this.name=name;
+            this.yPos = yPos;
+            this.xPos = xPos;
+        }
+        public void setAction(ActionCommand a)
+        {
+            this.action=a;
+        }
+        public void setPos(int x, int y)
+        {
+            this.yPos = y;
+            this.xPos = x;
+        }
+        public ActionCommand getAction()
+        {
+            return action;
+        }
+        public int getxPos()
+        {
+            return xPos;
+        }
+        public int getyPos()
+        {
+            return yPos;
+        }
+        public String getAssetIndentifer()
+        {
+            return assestID;
+        }
+        public void setAssetIndentifer(String assetID)
+        {
+            this.assetID = assetID;
+        }
+    }
+    /**
+     * <summary>A Real Entity is an abstract superclass used to represent objects that 'exsist' in the game</summary>
+     */
+    abstract class RealEntity : Entity
+    {
+        //interactabl probably not relevant to core data interaction- save for renderer
+        bool interactable;
+        public void setValues(String name, int xPos, int yPos, bool interactable)
+        {
+            setValues(name, xPos, yPos);
+            this.interactable = interactable;
+        }
+    }
+    /**
+     * <summary>An Abstract Entity is an abstract superclass used to represent orders & uses of mechanics on the battlemap</summary>
+     */
+    abstract class AbstractEntity : Entity
+    {
+        int turnDeployed;
+        public void setValues(String name, int xPos, int yPos,int turnDeployed)
+        {
+            setValues(name, xPos, yPos);
+            this.turnDeployed = turnDeployed;
+        }
+    }
+}
---
+++ DropGrid.Core/Entity/Unit.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DropGrid.Client.Entity;
+namespace DropGrid.Client.Entity
+{
+    /**
+     * 
+     * <summary>Unit is a type of </summary>
+     * 
+     */
+    class Unit:RealEntity
+    {
+        int pointsCost=0, damage=0, range=0, rangeSniper=0, health=4, assaultDamage=0;
+        Unit(String type,String name, int xPos, int yPos, bool interactable)
+        {
+            switch (type)
+            {
+                case "Infantry":
+                    name = "Infantry";
+                    pointsCost = 1;
+                    damage = 2;
+                    range = 1;
+                    health = 4;
+                    assaultDamage = 2;
+                    break;
+                default:
+                    break;
+            }
+
+
+        }
+    }
+}
