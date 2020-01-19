554d0e9b285587c50c90f8cebfa13d1b0ad12685
Starshiplad <starshipladdev@gmail.com>
2020-01-19 22:10:51 +1300

    Add 'Phase' class . This is an abstract class that handles an array of actions to perform on the the gamestate Add 'Turn' class. This is a list of phases to run through Waiting for confirmation from Nibby that this system is sufficent

---
+++ DropGrid.Core/TurnPhase/Class1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DropGrid.Core.TurnPhase
+//elisetrouw < band
+{
+    static  int phaseArraySize = 6;
+    static int reposnseCode = 0;
+    abstract class Phase
+    {
+        List<Enitity.Action> action = new List<Entity.Action>;
+        //performMechanics will return 1 if the action requires both players to send info.
+        //it returns 0 if it is an action the client can quite happily go to the next thing for
+        abstract int performMechanics()
+        {
+
+        }
+    }
+    class deploymentPhase : Phase
+    {
+        int performMechanics()
+        {
+            //As user clicks & creates user, edit Action list accordingly to send to server
+            
+            return 1;
+        }
+    }
+    class AttackPhase : Phase
+    {
+        public AttackPhase(List<Action> actions)
+        {
+            this.actions = actions;
+        }
+        //Example non->input phase. Run through given list of Actions and perform them
+        int performMechanics()
+        {
+
+            return 0;
+        }
+    }
+
+
+
+ 
+
+
+    class Turn
+    {
+        int deploymentPoints, movementPoints;
+        Phase[] phases = new Phase[phaseArraySize];
+        public void runTurn()
+        {
+            for(int i=0; i<phaseArraySize; i++)
+            {
+                if (phases[i].performMechanics() == 1)
+                {
+                    while(responseCode == 0)
+                    {
+                        //wait for a bit
+                        //Check with server again
+                    }
+                }
+            }
+        }
+       
+    }
+}
