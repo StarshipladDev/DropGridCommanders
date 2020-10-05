a6658992dab1df67cfdb3149af309b80cd2aa0a3
StarshipladDev <StarshipladDev@gmail.com>
2020-10-06 00:23:06 +1300

    06/10/2020 Version2.0 - MVP
    *Using new Monogame 3.8 Framework
    *Modular Pixel Art
    *Better Documentation

---
+++ Game1.cs
@@ -0,0 +1,138 @@
+﻿using DropGridCommanderVersion2.Units;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace DropGridCommanderVersion2
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch _spriteBatch;
+        static Texture2D GroundTile;
+        static Texture2D GroundTile1;
+        static Texture2D GroundTileForest;
+        static Texture2D selectionBorder;
+        static Texture2D[,] arrayTerrain = new Texture2D[10, 10];
+        private bool wPressed = false;
+        System.Drawing.Point selectionPoint = new System.Drawing.Point(0, 0);
+        Unit unitman; 
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+            //Setup Array
+            Random rand = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                for (int f = 0; f < 10; f++)
+                {
+                    Vector2 topLeftOfSprite = new Vector2(40 * i, 40 * f);
+                    Color tintColor = Color.White;
+                    if (rand.Next(4) > 2)
+                    {
+
+                        arrayTerrain[i, f] = GroundTile;
+                    }
+                    else if (rand.Next(4) > 1)
+                    {
+
+                        arrayTerrain[i, f] = GroundTile1;
+                    }
+                    else
+                    {
+
+                        arrayTerrain[i, f] = GroundTileForest;
+                    }
+                }
+            }
+
+            //Finish SetupArray
+
+            //SetHeight/Width
+            graphics.PreferredBackBufferWidth = 400;  // set this value to the desired width of your window
+            graphics.PreferredBackBufferHeight = 400;   // set this value to the desired height of your window
+            graphics.ApplyChanges();
+            String[] weapons = new string[] { "Bullpup","Sniper","Shotgun","Machinegun"};
+            unitman = new Unit(rand.Next(10), rand.Next(10) ,weapons[rand.Next(weapons.Length)], this.GraphicsDevice);
+            //End set height/width
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            System.IO.Stream stream;
+            stream = TitleContainer.OpenStream("Content/Ground_City.png");
+
+            GroundTile = Texture2D.FromStream(this.GraphicsDevice, stream);
+            stream.Dispose();
+            stream= TitleContainer.OpenStream("Content/Border.png");
+            selectionBorder = Texture2D.FromStream(this.GraphicsDevice, stream);
+            stream.Dispose();
+            stream = TitleContainer.OpenStream("Content/Ground_City2.png");
+
+            GroundTile1 = Texture2D.FromStream(this.GraphicsDevice, stream);
+            stream.Dispose();
+            stream = TitleContainer.OpenStream("Content/Ground_ForestTree.png");
+
+            GroundTileForest = Texture2D.FromStream(this.GraphicsDevice, stream);
+            stream.Dispose();
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            KeyboardState state = Keyboard.GetState();
+            if(state.IsKeyDown(Keys.W) && wPressed == false)
+            {
+                wPressed = true;
+                Random rand = new Random();
+                unitman= new Unit(rand.Next(10), rand.Next(10),"Bullpup",this.GraphicsDevice);
+            }
+            else if (state.IsKeyUp(Keys.W) && wPressed == true)
+            {
+                wPressed = false;
+
+            }
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            Random rand = new Random();
+            GraphicsDevice.Clear(Color.White);
+
+            _spriteBatch.Begin();
+            for(int i=0; i< 10; i++)
+            {
+                for(int f=0; f < 10; f++)
+                {
+                    Vector2 topLeftOfSprite = new Vector2(40*i,40*f);
+                    Color tintColor = Color.White;
+                    _spriteBatch.Draw(arrayTerrain[i,f], topLeftOfSprite, tintColor);
+                }
+            }
+            Vector2 v= new Vector2(selectionPoint.X, selectionPoint.Y);
+            Color color = Color.White;
+            _spriteBatch.Draw(selectionBorder, v, color);
+            unitman.Draw(_spriteBatch,ref arrayTerrain);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,13 +1,58 @@
-# DropGrid Commanders
+# Dropgrid Commander -> (Proffesional Collabrative Project)
 
-[Screenshot]:READMEImage.png
-[Kevinhref]:https://github.com/Nibby
-[Mehref]:https://github.com/StarshipladDev
-![This is Alt text][Screenshot]
+![Screenshot](READMEImage.png)
+**(Font is Banschift SemiLight)**
 
-This application is a collaborative project between [Kevin][Kevinhref] and [myself][Mehref]
-We plan on implementing a turn-based video game where all actions are declared before events occur, meaning all player's 
-units will act simultaneously. 
-The project will be updated in branches, and merged into master when agreed-upon implementations are realized.
-The project is still in the planning phase, and this README, along with proper documentation, will follow as the code base is built.
-Testing git
+This is a long-term project to between [Kevin][Kevinhref] and [myself][Mehref]to develop a turn-based multipalyer computer game.
+
+## Notes/Know Bugs:
+
+> Game is currently in an unplayable state
+
+## Features(Planned In Brackets)
+
+> (Custom pixel art for units, terrain and events)
+
+> (Multiplayer functionality)
+
+> (Cheat-checking)
+
+> (A.I bots)
+
+
+## View of progress
+
+No Image yet
+
+## Latest Build
+Still In progress
+
+## Latest Update
+
+06/10/2020 - DropgridCommander 2.0 
+
+*Using new Monogame 3.8 Framework
+*Modular Pixel Art
+*Better Documentation
+
+## Next Build
+
+Week ending 30/11/2020 -Release 0.1a - Version 2 using monogame 3.8
+
+* Add Randomly Generated Grid
+
+* Add clicking unity on grid, allowing them to move to adjacent grids
+
+
+## Skill developing
+
+I plan on this project improving my skills in the following:
+
+> C#
+
+> Source control and git
+
+>Pixel Art and Animation
+
+## Installing and Compiling:
+Project not yet installable 
